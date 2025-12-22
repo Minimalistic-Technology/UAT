6a866978576ba641f3a9c783282dcf3bdc7b1a75
Manan1884 <manan18doshi@gmail.com>
2025-12-23 00:42:50 +0530

    Commit

--- services/minimalistic-learning/src/app.ts
+++ services/minimalistic-learning/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDatabase } from './config/db';  
 // import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -11,7 +12,16 @@ import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
 
 const app = express();
-app.set('trust proxy', 1);
+
+(async () => {
+  try {
+    await connectDatabase();
+  } catch (err) {
+    console.error(' MongoDB connection failed', err);
+    process.exit(1);
+  }
+})();
+
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({
