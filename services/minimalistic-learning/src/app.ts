e0b857c574c18921c4caa6b9038de0c47a94c0cb
Manan1884 <manan18doshi@gmail.com>
2025-12-23 00:34:03 +0530

    Initial Commit

--- services/minimalistic-learning/src/app.ts
+++ services/minimalistic-learning/src/app.ts
@@ -11,7 +11,7 @@ import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
 
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({
