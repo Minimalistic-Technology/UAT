cfa2c95adb74b500f8a2120c3c6b4a80a3d87c68
Manan1884 <manan18doshi@gmail.com>
2025-12-23 16:30:26 +0530

    CORS Changes

--- services/minimalistic-learning/src/app.ts
+++ services/minimalistic-learning/src/app.ts
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: 'https://minimalistic-learning.onrender.com', 
     credentials: true,                
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   }));
 
 app.get('/health', (_req, res) => {
