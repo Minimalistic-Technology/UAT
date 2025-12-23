6be57caf47a4123c72963d534321581120bbd689
Meet sanwadkar <mrsanwadkar@gmail.com>
2025-12-23 22:42:43 +0530

    cookie changes

--- services/minimalistic-hrm/src/controllers/authAccessController.ts
+++ services/minimalistic-hrm/src/controllers/authAccessController.ts
@@ -16,8 +16,8 @@ const createToken = (id: string, role: string) => {
 const sendTokenResponse = (res: Response, token: string) => {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure:true,
+    sameSite:"none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
