9a9bf2fef8b942ea4e4aca1a86575e43fdc30e69
Raj Mane <rajmane5884@gmail.com>
2026-07-01 18:39:22 +0530

    feat: implement node cluster module to enable multi-core utilization in backend services

--- services/minimalistic-learning/ml-backend/src/server.ts
+++ services/minimalistic-learning/ml-backend/src/server.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import cluster from "cluster";
+import os from "os";
 import app from "./app";
 import { connectDatabase } from './config/db';
 import { env } from './config/env';
@@ -21,5 +23,20 @@ const startServer = async () => {
     process.exit(1);
   }
 };
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  console.log(`[server] Primary ${process.pid} is running`);
+  console.log(`[server] Forking ${numCPUs} workers...`);
 
-startServer();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`[server] Worker ${worker.process.pid} died (code: ${code}, signal: ${signal}). Restarting...`);
+    cluster.fork();
+  });
+} else {
+  startServer();
+  console.log(`[server] Worker ${process.pid} started`);
+}
