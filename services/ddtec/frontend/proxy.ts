5a489f83fda4aaaf5a10266f86c1d85e85c489de
Raj Mane <rajmane5884@gmail.com>
2026-08-20 17:08:58 +0530

    comming soon fix

--- services/ddtec/frontend/proxy.ts
+++ services/ddtec/frontend/proxy.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 const COMING_SOON_PATH = "/coming-soon";
 
 export function proxy(request: NextRequest) {
-  const isComingSoon = true;
+  const isComingSoon = false;
   if (!isComingSoon) {
     return NextResponse.next();
   }
